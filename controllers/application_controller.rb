034fc8ac6d3d4b6fa36a0e1439abc98c004db916
CarsonBills <CarsonMBills@Gmail.com>
2015-03-11 18:16:16 -0400

    enables sessions

--- controllers/application_controller.rb
+++ controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < Sinatra::Base
 	set :views, File.expand_path('../../views', __FILE__)
 	set :public_folder, File.expand_path("../../public", __FILE__)
 
+  enable :sessions, :method_override
+
 	get "/" do
 		erb :index
 	end
