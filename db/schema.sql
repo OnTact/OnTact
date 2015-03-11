5300488ef2bd0f4fc3929774aa51d067ea0dd42b
gretchenziegler <gretchenziegler@gmail.com>
2015-03-11 12:13:42 -0400

    one line schema for easy copy-pasting

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,5 @@
+# one line schema
+
 CREATE TABLE users (id serial primary key, f_name varchar(255), l_name varchar(255), username varchar(255), password_digest text, updated_at timestamp, created_at timestamp, personal_email text, professional_email text, phone varchar(50), account_email text, company varchar(255), position varchar(255), website text, image integer, card_layout integer, connection_count integer, points integer
 );
 
@@ -6,3 +8,4 @@ CREATE TABLE connections (id serial primary key, initiator_id integer, receiver_
 
 CREATE TABLE leads (id serial primary key, email text, first_name text, created_at timestamp, updated_at timestamp
 );
+
