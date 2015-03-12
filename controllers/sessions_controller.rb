58771816a97d247def150dffa3b52bb7671ceed3
CarsonBills <CarsonMBills@Gmail.com>
2015-03-12 14:51:02 -0400

    adds logout button and functionality, removes excess binding.pry

--- controllers/sessions_controller.rb
+++ controllers/sessions_controller.rb
@@ -2,15 +2,17 @@ class SessionsController < ApplicationController
 
 	post '/' do
 		user = User.find_by({username: params[:username]})
-    binding.pry
     if user && user.password == params[:password]
       session[:current_user] = user.id
-      binding.pry
       redirect "/user/#{user.id}"
     else
-    	binding.pry
       redirect '/'
     end
 	end
+
+	delete '/' do
+    session[:current_user] = nil
+    redirect '/'
+  end
 
 end
