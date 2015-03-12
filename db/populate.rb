7ab3803178582ea9eb1eb3852f1d8e7baec0b0da
Sam Callender <samcal84@gmail.com>
2015-03-12 17:15:12 -0400

    poulate.rb file for DB tables

---
+++ db/populate.rb
@@ -0,0 +1,47 @@
+require 'pg'
+require 'pry'
+
+init_users = <<-SQL
+CREATE TABLE users(
+	id	SERIAL	PRIMARY KEY,
+	f_name VARCHAR(255),
+	l_name VARCHAR(255),
+	username VARCHAR(255),
+	password_digest TEXT,
+	account_email TEXT,
+	updated_at TIMESTAMP,
+	created_at TIMESTAMP,
+	personal_email TEXT,
+	personal_phone VARCHAR,
+	professional_email TEXT,
+	phone VARCHAR(50),
+	company VARCHAR(255),
+	website TEXT,
+	image INTEGER,
+	card_layout INTEGER,
+	connection_count INTEGER,
+	points INTEGER
+	);
+SQL
+
+init_connections = <<-SQL
+CREATE TABLE connections(
+	id SERIAL PRIMARY KEY,
+	initiator_id INTEGER,
+	receiver_id INTEGER,
+	created_at INTEGER,
+	updated_at INTEGER,
+	pending BOOLEAN,
+	connection_type VARCHAR(255),
+	is_connected BOOLEAN,
+	additional_info TEXT
+	);
+SQL
+
+db = PG.connect(dbname: 'ontact')
+
+db.exec('DROP TABLE IF EXISTS users')
+db.exec('DROP TABLE IF EXISTS connections')
+
+db.exec(init_users)
+db.exec(init_connections)
