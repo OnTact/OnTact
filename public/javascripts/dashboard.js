9b21572d2a8eb9ea831beae290a781b9413b62f3
gretchenziegler <gretchenziegler@gmail.com>
2015-03-13 18:31:51 -0400

    added event listeners to dashboard buttons...and sam prettified nav

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -1,9 +1,38 @@
-var $container = $('#dash-container')
+console.log('linked');
 
-var 
+var $container = $('#dash-content')
 
 $(function(){	
 
+	// render profile
+	$("#profile-bttn").on('click', function(){
+		console.log('clicked');
+		$container.empty();
+		var profileTemplate = $("#profile-view-template");
+		$container.append(profileTemplate.html());
+	});	
 
+	// render all contacts
+	$("#connections-bttn").on('click', function(){
+		console.log('clicked');
+		$container.empty();
+		var connectionsTemplate = $("#connections-all-template");
+		$container.append(connectionsTemplate.html());
+	});	
 
-})
+	// render notifications
+	$("#notifications-bttn").on('click', function(){
+		console.log('clicked');
+		$container.empty();
+		var notificationsTemplate = $("#notifications-view-template");
+		$container.append(notificationsTemplate.html());
+	});	
+
+	// render add contact form
+	$("#add-connection-bttn").on('click', function(){
+		console.log('clicked');
+		$container.empty();
+		var addTemplate = $("#add-connection-template");
+		$container.append(addTemplate.html());
+	});	
+});
